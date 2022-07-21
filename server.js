ed2dbd320f920c8789128a5da70be96efcf82bc9
Lortiz528 <lortiztech@gmail.com>
2022-07-21 12:38:28 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,27 @@
+//Dependencies
+const express = require("express");
+const app = express();
+const cors = require('cors')
+
+require("dotenv").config();
+
+//Port
+// Allow use of Heroku's port or your own local port, depending on the environment
+const PORT = process.env.PORT || 3003;
+
+//___________________
+//Middleware
+//___________________
+
+app.use(express.json()); // returns middleware that only parses JSON
+
+// this allows any app/site from anywhere access your API. This is a great way to start to get things up and running. Later, add restrictions, as needed.
+app.use(cors());
+
+// Routes
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+//Listen
+app.listen(PORT, () => console.log("Listening on port:", PORT));
