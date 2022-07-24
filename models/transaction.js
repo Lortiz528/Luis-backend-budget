10d1ec453d7592a5b24be93cfd17798f243f3199
Lortiz528 <lortiztech@gmail.com>
2022-07-24 12:35:54 -0400

    edits to model

--- models/transaction.js
+++ models/transaction.js
@@ -1,46 +1,58 @@
 module.exports = [
-    {
-      item_name: "paycheck",
-      amount: 1500,
-      date: "07/21/2022",
-      from: "work",
-      category: "income",
-    },
-    {
-      item_name: "gas",
-      amount: -50,
-      date: "07/21/2022",
-      from: "gas station",
-      category: "vehicle",
-    },
-    {
-      item_name: "savings",
-      amount: -500,
-      date: "07/21/2022",
-      from: "bank",
-      category: "savings",
-    },
-    {
-        item_name: "grocery haul",
-        amount: -200,
-        date: "07/21/2022",
-        from: "supermarket",
-        category: "food",
-      },
-      {
-        item_name: "rent",
-        amount: -750,
-        date: "07/01/2022",
-        from: "bank",
-        category: "housing",
-      }
-  ];
+  {
+    item_name: 'Paycheck',
+    amount: 1500,
+    date: '07/21/2022',
+    from: 'work',
+    category: 'Income',
+    type: 'income',
+  },
+  {
+    item_name: 'Fuel',
+    amount: -50,
+    date: '07/21/2022',
+    from: 'gas station',
+    category: 'vehicle',
+    type: 'expense',
+  },
+  {
+    item_name: 'Savings',
+    amount: -500,
+    date: '07/21/2022',
+    from: 'bank',
+    category: 'savings',
+    type: 'expense',
+  },
+  {
+    item_name: 'Cell phone',
+    amount: -70,
+    date: '07/15/2022',
+    from: 'T-Mobile',
+    category: 'phone',
+    type: 'expense',
+  },
+  {
+    item_name: 'Grocery haul',
+    amount: -200,
+    date: '07/21/2022',
+    from: 'grocery store',
+    category: 'food',
+    type: 'expense',
+  },
+  {
+    item_name: 'Rent',
+    amount: -750,
+    date: '07/01/2022',
+    from: 'bank',
+    category: 'housing',
+    type: 'expense',
+  },
+];
 
-  /*
+/*
   item_name- string
   amount- number
   date- string
   from- string
   category - string
   */
-  
