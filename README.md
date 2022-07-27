056ed85cb02c3213a66280515b16a2f12bb23b5c
Lortiz528 <lortiztech@gmail.com>
2022-07-27 10:47:27 -0400

    font and link to repo

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Luis-backend-budget
+
+# Links Below
+
+- [Frontend Site](https://luis-budget-manager.netlify.app)
+
+- [Backend](https://luis-budget-backend-app.herokuapp.com)
+
+- [Project Prompt](https://github.com/joinpursuit/budgeting-app-project-prompt)
--- app.js
+++ app.js
@@ -9,7 +9,8 @@ app.use(express.json());
 const transactionController = require('./controllers/transactions');
 
 app.get('/', (req, res) => {
-  res.send('Welcome to the budget app!');
+  res.send(`<h1 style="text-align:center;font-family: 'Courier New', Courier, monospace;">Welcome to Budget Budget Backend!</h1>
+  <p style="text-align:center; font-family: 'Courier New', Courier, monospace;"><a href="https://github.com/Lortiz528/Luis-backend-budget">Link to the Repo</a></p>`);
 });
 
 app.use('/transactions', transactionController);
@@ -17,7 +18,5 @@ app.use('/transactions', transactionController);
 app.use('/*', (req, res) => {
   res.status(404).send('Not Found!');
 });
-
-
 
 module.exports = app;
